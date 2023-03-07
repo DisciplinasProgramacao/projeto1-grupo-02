de37963b063cf95701ce79266565a24c9b77417a
André Rodrigues <mrnexusrider@hotmail.com>
2023-03-07 10:24:08 -0300

    Melhoria na classe Produto

--- codigo/src/business/Produto.java
+++ codigo/src/business/Produto.java
@@ -1,24 +1,24 @@
 package business;
 
 public class Produto {
-    private int codProduto;
+    private int codProduto;  //OK
     private int totalEmEstoque;
     private boolean estaAbaixoDoLimite;
-    private String descricaoDoProduto;
+    private int estoqueMinimo;
+    private String descricaoDoProduto;  //OK
     private float precoDeCusto;
-    private float precoDeVenda;
-    private float margemLucro;
-    private float valorImposto;
-    private int qtdAdquirida;
-    private int qtdVendida;
+    private float precoDeVenda;  //OK
+    private float margemLucro;  //OK
+    private float valorImposto;  //OK
+    private int qtdAdquirida = 0;
+    private int qtdVendida = 0;
     private float totalGasto;
-    private float totalArrecadado;
+    private float totalArrecadado = 0;
 
     private static int contador = 1;
 
-    public Produto(int codProduto, int totalEmEstoque, boolean estaAbaixoDoLimite, String descricaoDoProduto,
-            float precoDeCusto, float precoDeVenda, float margemLucro, float valorImposto, int qtdAdquirida,
-            int qtdVendida, float totalGasto, float totalArrecadado) {
+    public Produto(int codProduto, int totalEmEstoque, int estoqueMinimo, String descricaoDoProduto,
+            float precoDeCusto, float valorImposto, float totalGasto) {
         setDescricaoDoProduto(descricaoDoProduto);
         setCodProduto();
         setMargemLucro();
@@ -26,7 +26,16 @@ public class Produto {
         setPrecoDeVenda();
     }
 
-    public void setDescricaoDoProduto(String descricaoDoProduto) {
+    public float adquirirProduto(int qtdAdquirida){
+
+    }
+
+    public float venderProduto(int qtdVendida){
+        
+    }
+
+
+    private void setDescricaoDoProduto(String descricaoDoProduto) {
         if (descricaoDoProduto.length() >= 3) {
             this.descricaoDoProduto = descricaoDoProduto;
         } else {
@@ -34,19 +43,85 @@ public class Produto {
         }
     }
 
-    public void setPrecoDeVenda() {
+    private void setPrecoDeVenda() {
         this.precoDeVenda = this.precoDeCusto + this.margemLucro + this.valorImposto;
     }
 
-    public void setCodProduto() {
+
+    private void setCodProduto() {
         this.codProduto = contador++;
     }
 
-    public void setMargemLucro() {
+    private void setMargemLucro() {
         this.margemLucro = (0.55f * this.precoDeCusto);
     }
 
-    public void setValorImposto() {
+    private void setValorImposto() {
         this.valorImposto = (this.precoDeCusto + this.margemLucro) * 0.18f;
     }
+
+    public void setEstoqueMinimo(int estoqueMinimo){
+        this.estoqueMinimo = estoqueMinimo;
+    }
+
+
+    public int getCodProduto() {
+        return this.codProduto;
+    }
+
+
+    public int getTotalEmEstoque() {
+        return this.totalEmEstoque;
+    }
+
+    public String getDescricaoDoProduto() {
+        return this.descricaoDoProduto;
+    }
+
+
+    public float getPrecoDeCusto() {
+        return this.precoDeCusto;
+    }
+
+
+    public float getPrecoDeVenda() {
+        return this.precoDeVenda;
+    }
+
+
+    public float getMargemLucro() {
+        return this.margemLucro;
+    }
+
+
+    public float getValorImposto() {
+        return this.valorImposto;
+    }
+
+
+    public int getQtdAdquirida() {
+        return this.qtdAdquirida;
+    }
+
+    
+
+    public int getQtdVendida() {
+        return this.qtdVendida;
+    }
+
+    
+
+    public float getTotalGasto() {
+        return this.totalGasto;
+    }
+
+    
+
+    public float getTotalArrecadado() {
+        return this.totalArrecadado;
+    }
+
+    
+    
+
 }
