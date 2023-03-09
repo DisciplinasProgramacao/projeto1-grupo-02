ee1a1bdf1c4144c5c2479610df7bdcbd55d57ebc
0xLott <llott1710@gmail.com>
2023-03-09 00:26:34 -0300

    Atualizado Estoque

--- codigo/src/business/Estoque.java
+++ codigo/src/business/Estoque.java
@@ -6,10 +6,41 @@ import java.util.Map;
 public class Estoque {
 	
 	private Map<Produto, Integer> produtos;
+
+	// TODO: adicionar os seguintes atributos ao diagrama
+	private float valorTotal = 0; 
+	private int qtdTotalProdutos = 0; 
+
 	
 	public Estoque() {
 		produtos = new HashMap<>();
 	}
+
+	public float calcularValorTotal(){
+		produtos.forEach((key, value) -> valorTotal =+ key.getPrecoDeVenda());
+		return valorTotal;
+	}
+
+	public int qtdTotalProdutos(){
+		produtos.forEach((key, value) -> qtdTotalProdutos++);
+		return qtdTotalProdutos;
+	}
+
+	// TODO: Alterar no diagrama de Estoque os parâmetros do método
+	public void adicionarProduto(Produto produtoAdicionado, int qtdAdquirida){
+		produtoAdicionado.adquirirProduto(qtdAdquirida);
+		produtos.put(produtoAdicionado, qtdAdquirida);
+	}
+
+	public void removerProduto(Produto produtoRemovido) throws Exception{
+		if (produtos.containsKey(produtoRemovido)){
+			produtos.remove(produtoRemovido);
+			System.out.println("Produto removido: " + produtoRemovido.getDescricaoDoProduto());
+		} else {
+			throw new NullPointerException();
+		}
+		
+	};
 	
 	public void reporEstoque(Produto produtoReposto, int qtd) {
 		if (qtd > 0) {
@@ -17,7 +48,7 @@ public class Estoque {
 		}
 	}
 	
-	public void retirarEstoque(Produto produtoRetirado, int qtd) {
+	public void retirarEstoque(Produto produtoRetirado, int qtd) { 
 		if (qtd > 0) {
 			if (getEstoque().get(produtoRetirado) == null || getEstoque().get(produtoRetirado) == 0) {
 				return;
--- codigo/src/business/EstoqueTest.java
+++ codigo/src/business/EstoqueTest.java
@@ -1,73 +1,106 @@
 package business;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class EstoqueTest {
 	
 	Estoque estoque;
-	Produto produto;
+	Produto produto1, produto2;
 	
     @BeforeEach
     public void prepare() {
     	estoque = new Estoque();
-    	produto = new Produto("Shampoo", 5, 100);
+
+    	produto1 = new Produto("Shampoo", 5, 100);
+		produto2 = new Produto("Livro", 100, 20);
+
+		estoque.adicionarProduto(produto2, 50);
     }
 
 	@Test
+	void tentaCalcularValorTotalDoEstoque(){
+		assertEquals(36.58F, estoque.calcularValorTotal());
+	}
+
+	@Test
+	void tentaCalcularQtdTotalDeProdutos(){
+		assertEquals(1, estoque.qtdTotalProdutos());
+	}
+
+	@Test
+	void tentaAdicionarProduto(){
+		Produto produto3 = new Produto("Caixa", 10, 5);
+		estoque.adicionarProduto(produto3, 1);
+		assertEquals(2, estoque.qtdTotalProdutos());
+	}
+
+	@Test
+	void tentaRemoverProdutoExistente() throws Exception{
+		estoque.removerProduto(produto2);
+		assertEquals(0, estoque.qtdTotalProdutos(), "Remoção de produto existente no estoque");
+	}
+
+
+	@Test
+	void tentaRemoverProdutoInexistente(){
+		assertThrows(NullPointerException.class, () -> estoque.removerProduto(produto1));
+	}
+
+	@Test
 	void tentaReporEstoqueComValorNegativo() {
-		estoque.reporEstoque(produto, -5);
-		assertEquals(null, estoque.getEstoque().get(produto));
+		estoque.reporEstoque(produto1, -5);
+		assertEquals(null, estoque.getEstoque().get(produto1));
 	}
 	
 	@Test
 	void tentaReporEstoqueComValorZero() {
-		estoque.reporEstoque(produto, 0);
-		assertEquals(null, estoque.getEstoque().get(produto));
+		estoque.reporEstoque(produto1, 0);
+		assertEquals(null, estoque.getEstoque().get(produto1));
 	}
 	
 	@Test
 	void tentaReporEstoqueComValorPositivo() {
-		estoque.reporEstoque(produto, 5);
-		assertEquals(5, estoque.getEstoque().get(produto));
+		estoque.reporEstoque(produto1, 5);
+		assertEquals(5, estoque.getEstoque().get(produto1));
 	}
 	
 	@Test
 	void tentaReporEstoqueComValorPrevio() {
-		estoque.reporEstoque(produto, 5);
-		estoque.reporEstoque(produto, 5);
-		assertEquals(10, estoque.getEstoque().get(produto));
+		estoque.reporEstoque(produto1, 5);
+		estoque.reporEstoque(produto1, 5);
+		assertEquals(10, estoque.getEstoque().get(produto1));
 	}
 	
 	// Retiradas
 	
 	@Test
 	void tentaRetirarEstoqueComValorNegativo() {
-		estoque.reporEstoque(produto, 100);
-		estoque.retirarEstoque(produto, -5);
-		assertEquals(100, estoque.getEstoque().get(produto));
+		estoque.reporEstoque(produto1, 100);
+		estoque.retirarEstoque(produto1, -5);
+		assertEquals(100, estoque.getEstoque().get(produto1));
 	}
 	
 	@Test
 	void tentaRetirarEstoqueComValorZero() {
-		estoque.reporEstoque(produto, 100);
-		estoque.reporEstoque(produto, 0);
-		assertEquals(100, estoque.getEstoque().get(produto));
+		estoque.reporEstoque(produto1, 100);
+		estoque.reporEstoque(produto1, 0);
+		assertEquals(100, estoque.getEstoque().get(produto1));
 	}
 	
 	@Test
 	void tentaRetirarEstoqueComValorPositivo() {
-		estoque.reporEstoque(produto, 100);
-		estoque.retirarEstoque(produto, 5);
-		assertEquals(95, estoque.getEstoque().get(produto));
+		estoque.reporEstoque(produto1, 100);
+		estoque.retirarEstoque(produto1, 5);
+		assertEquals(95, estoque.getEstoque().get(produto1));
 	}
 	
 	@Test
 	void tentaRetirarEstoqueMaisDoQuePossivel() {
-		estoque.reporEstoque(produto, 100);
-		estoque.retirarEstoque(produto, 150);
-		assertEquals(0, estoque.getEstoque().get(produto));
+		estoque.reporEstoque(produto1, 100);
+		estoque.retirarEstoque(produto1, 150);
+		assertEquals(0, estoque.getEstoque().get(produto1));
 	}
 }
