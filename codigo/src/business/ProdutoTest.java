28f3f23162c409eed7efb6e9e27e40e574dbbcb5
Gustavo <104271289+GustavoAndradeGit@users.noreply.github.com>
2023-03-13 22:36:48 -0300

    Update ProdutoTest.java

--- codigo/src/business/ProdutoTest.java
+++ codigo/src/business/ProdutoTest.java
@@ -4,44 +4,64 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+/**
+ * TESTES:
+ * 	Criando o produto:
+ 		- O codigo do produto deve ser gerado adicionando 1 ao codigo do produto criado anteriormente ✅
+ 		- O valor do lucro deve ser calculado com 0.55 * Preco de custo ✅
+ 		- O valor de imposto deve ser calculado com 0.18 * (preco de custo + lucro) ✅
+    Deve ser possivel adicionar ou vender produtos do estoque
+        - Ao ser adicionado o estoque e valor gasto sap atualizados ✅
+        - Ao ser vendido o estoque e valor adquirido sao atualizados ✅
+        
+        
+ * 
+ * 
+ */
+
+
 class ProdutoTest {
 
-    Produto meuProduto;
-
-    @BeforeEach
-    public void prepare() {
-        meuProduto = new Produto(20, 60, false, "Descricao do produto", (float) 20.00, (float) 40.00, (float) 15.00,
-                (float) 5.00, 10, 5, (float) 100.00, (float) 60.00);
-    }
-
-    @Test
-    public void validaDescricaoErrado() {
-        meuProduto.setDescricaoDoProduto("d");
-        assertEquals("d", meuProduto.getDescricaoDoProduto());
-    }
-
-    @Test
-    public void validaDescricaoCerto() {
-        meuProduto.setDescricaoDoProduto("Descricao valida");
-        assertEquals("Descricao valida", meuProduto.getDescricaoDoProduto());
-    }
-
-    @Test
-    public void conferePrecoDeVenda() {
-        meuProduto.setPrecoDeVenda();
-        assertEquals(40.0, meuProduto.getPrecoDeVenda());
-    }
-
-    @Test
-    public void verificaMargemDeLucro() {
-        meuProduto.setMargemLucro();
-        assertEquals(11, meuProduto.getMargemLucro());
-    }
-
-    @Test
-    public void verificaValorImposto() {
-        meuProduto.setValorImposto();
-        assertEquals(4.5, meuProduto.getValorImposto());
-    }
-
+	Produto meuProduto;
+	Produto meuNovoProduto;
+	
+	@BeforeEach
+	public void prepare() {
+		meuProduto = new Produto("Descricao do produto", 2, 20);
+		meuNovoProduto = new Produto("Segundo produto", 15, 30);
+		meuProduto.adquirirProduto(10);
+		
+	}
+	
+	@Test
+	public void confereCodigoDeProduto() {
+		assertEquals(1, meuProduto.getCodProduto());
+		assertEquals(2, meuNovoProduto.getCodProduto());
+	}
+	
+	@Test
+	public void confereMargemDeLucro() {
+		assertEquals(11, meuProduto.getMargemLucro());
+	}
+	
+	@Test
+	public void confereValorImposto() {
+		assertEquals(5.58, meuProduto.getValorImposto());
+	}
+	
+	@Test
+	public void confereAdquirirProdutos() {
+		assertEquals(10, meuProduto.getTotalEmEstoque());
+		assertEquals(10, meuProduto.getQtdAdquirida());
+		assertEquals(200, meuProduto.getTotalGasto());
+	}
+	
+	@Test
+	public void confereVenderProdutos() {
+		meuProduto.venderProduto(8);
+		assertEquals(2, meuProduto.getTotalEmEstoque());
+		assertEquals(8, meuProduto.getQtdVendida());
+		assertEquals(292.64, meuProduto.getTotalArrecadado());
+	}
+ 
 }
